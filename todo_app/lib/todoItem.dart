ec385bab8463955feb29189bc36c5758c298acc4
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-07-16 13:31:12 +0300

    Change the some features for api response data

--- todo_app/lib/todoItem.dart
+++ todo_app/lib/todoItem.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/constants/taskType.dart';
 import 'package:todo_app/model/task.dart';
+import 'package:todo_app/model/todos.dart';
 
 class Todoitem extends StatefulWidget {
   const Todoitem({super.key, required this.task});
-  final Task task;
+  final Todo task;
   @override
   State<Todoitem> createState() => _TodoitemState();
 }
@@ -14,29 +15,32 @@ class _TodoitemState extends State<Todoitem> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: widget.task.isCompleted==true? const Color.fromARGB(255, 182, 181, 181) : Colors.white,
+      color: widget.task.completed==true? const Color.fromARGB(255, 182, 181, 181) : Colors.white,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       child: Padding(
         padding: const EdgeInsets.all(20),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-
+              /* firebase işlemlerin de tekrar kullanılacak
             widget.task.type == Tasktype.note 
             ? Image.asset("lib/assets/images/category_1.png") 
             : widget.task.type == Tasktype.constest 
               ? Image.asset("lib/assets/images/category_2.png")
               : Image.asset("lib/assets/images/category_3.png"),
-            
+            */
+            Image.asset("lib/assets/images/category_1.png"),
             Expanded(
               child: Column(children: [
                 Text(
-                  widget.task.title,
+                  widget.task.todo!,
                   style:
-                       TextStyle(fontWeight: FontWeight.bold, fontSize: 21,
-                      decoration: widget.task.isCompleted== true? TextDecoration.lineThrough : TextDecoration.none),
+                       TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 21,
+                        decoration: widget.task.completed== true? TextDecoration.lineThrough : TextDecoration.none),
                 ),
-                 Text(widget.task.description,style: TextStyle(decoration: widget.task.isCompleted== true? TextDecoration.lineThrough : TextDecoration.none),)
+                 Text("User : ${widget.task.userId}" ,style: TextStyle(decoration: widget.task.completed== true? TextDecoration.lineThrough : TextDecoration.none),)
               ],
                          ),
             ),
@@ -44,8 +48,8 @@ class _TodoitemState extends State<Todoitem> {
                 value: isCheck,
                 onChanged: (val) => {
                       setState(() {
-                        widget.task.isCompleted = !widget.task.isCompleted;
                         isCheck = val!;
+                        widget.task.completed = !widget.task.completed!;  
                       })
                     }),
           ],
